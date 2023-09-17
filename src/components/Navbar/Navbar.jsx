78244a26e6ddbd86a0c50adf6aadcd0226f31225
Mohamed Abdellah <94642450+Mohamed-Abdellah89@users.noreply.github.com>
2023-09-17 15:59:16 +0300

    finished Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import {React, useState} from 'react'
 import './Navbar.css';
+
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from 'react-icons/md';
+
+import images from '../../constants/images';
+
+
+
+
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt="app_logo" />
+
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href="#home">Home</a></li>
+        <li className='p__opensans'><a href="#about">About</a></li>
+        <li className='p__opensans'><a href="#menu">Menu</a></li>
+        <li className='p__opensans'><a href="#award">Award</a></li>
+        <li className='p__opensans'><a href="#contact">Contact</a></li>
+
+      </ul>
+
+      <div className='app__navbar-login'>
+        <a href="#login" className='p__opensans'>Log In / Register</a>
+
+        <div/>
+
+        <a href="/" className='p__opensans'>Book Table</a>
+
+      </div>
+
+        <div className='app__navbar-smallscreen'>
+          <GiHamburgerMenu color ='#fff' fontSize={27} onClick={() => setToggleMenu(true)}/>
+          {
+            toggleMenu && (
+              
+              <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+                  <MdOutlineRestaurantMenu className='overlay__close' fontSize={27} onClick={() => setToggleMenu(false)}/>
+                  <ul className='app__navbar-smallscreen_links'>
+                    <li><a onClick={() => setToggleMenu(false)} href="#home">Home</a></li>
+                    <li><a onClick={() => setToggleMenu(false)} href="#about">About</a></li>
+                    <li><a onClick={() => setToggleMenu(false)} href="#menu">Menu</a></li>
+                    <li><a onClick={() => setToggleMenu(false)} href="#award">Award</a></li>
+                    <li><a onClick={() => setToggleMenu(false)} href="#contact">Contact</a></li>
+
+                  </ul>
+                  <div className='app__navbar-smallscreen_login'>
+                    <a href="#login" className='p__opensans'>Log In / Register</a>
+
+                    
+
+                    <a href="/" className='p__opensans'>Book Table</a>
+
+                  </div>
+              </div>
+            )
+          }
+
+        </div>
+
+    </nav>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,12 @@ import SubHeading from './SubHeading/SubHeading';
 import FooterOverlay from './Footer/FooterOverlay';
 import Newslettre from './Footer/Newsletter';
 import Navbar from './Navbar/Navbar';
-import MenuItem from './Menuitem/Menuitem';
+import Menuitem from './Menuitem/Menuitem';
 
 export{
    SubHeading,
    FooterOverlay,
    Navbar,
    Newslettre,
-   MenuItem 
+   Menuitem 
 };
