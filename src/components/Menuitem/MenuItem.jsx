baa4ca4485b11225138648d200a194d96a912a57
Mohamed Abdellah <94642450+Mohamed-Abdellah89@users.noreply.github.com>
2023-09-24 00:42:57 +0300

    edit & finished SpecialMenu

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import './MenuItem.css'
-const Menuitem = () => {
+const MenuItem = ({title, price, tags}) => {
   return (
-    <div>
-        Menuitem
+    <div className='app__menuItem'>
+      <div className='app__menuItem-head'>
+
+        <div className='app__menuItem-head_name'>
+          <p className='p__cormorant'>{title}</p>
+
+        </div>
+
+        <div className='app__menuItem-dash'/>
+
+        <div className='app__menuItem-head_price'>
+          <p className='p__cormorant'>{price}</p>
+
+        </div>
+
+      </div>
+
+      <div className='app__menuItem-head_sub'>
+      <p className='p__opensans'>{tags}</p>
+      </div>
+      
     </div>
   )
 }
 
-export default Menuitem
+export default MenuItem
