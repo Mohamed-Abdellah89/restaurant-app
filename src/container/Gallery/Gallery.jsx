7955cb22f8375462bcf9d6f0731d5a105b0a6621
Mohamed Abdellah <94642450+Mohamed-Abdellah89@users.noreply.github.com>
2023-12-04 17:35:11 +0200

    edit gallery

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from 'react';
+import {BsInstagram, BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs';
+import {SubHeading} from '../../components';
+import {images} from '../../constants';
 import './Gallery.css';
+
+const Images = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+
+
+
+
 const Gallery = () => {
+
+const scrollRef = React.useRef(null);
+
+const scroll = (direction) => {
+  const {current} = scrollRef;
+
+  if (direction === 'left'){
+    current.scroll -=300;
+
+  }
+  else{
+    current.scroll +=300;
+  }
+
+}
+
+
+
+
   return (
-    <div>
-      Gallery
+    <div className='app__gallery flex__center'>
+
+      <div className='app__gallery-content'>
+        <SubHeading title= "Instagram" />
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button className='custom__button'>View More</button>
+      </div>
+
+      <div className='app__gallery-images'>
+
+        <div className='app__gallery-images_container' ref={scrollRef}>
+          {
+            Images.map((image, index) => (
+              <div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}>
+                <img src={image} alt="gallery_image" />
+                <BsInstagram className='gallery__image-icon'/>
+              </div>
+            ))
+          }
+
+        </div>
+
+        <div className='app__gallery-images_arrow'>
+
+          <BsArrowLeftShort className='arrow-icon' onClick={() => scroll('left')}/>
+          <BsArrowRightShort className='arrow-icon' onClick={() => scroll('right')}/>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
